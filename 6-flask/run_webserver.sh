df21d997f8a0b9f2c3ac054a2ac1fe8ed84d0d6f
Andrey Val <hhdusha@gmail.com>
2025-04-15 14:45:30 +0300

    rename

--- 6-flask/run_webserver.sh
+++ 6-flask/run_webserver.sh
@@ -1,11 +1,11 @@
 ############################
 # ручной запуск
 #
-# на сервере запускать 
+# на сервере запускать
 #   > cd docker
 #   > docker-compose up
 ###
 #source $(pwd)/../venv/bin/activate
 source $(pwd)/../py11_3_env/bin/activate
-# gunicorn 3:app -w 4 -b 134.209.194.133:5000
-gunicorn 3:app -w 4 -b 127.0.0.1:5000
+# gunicorn web_app:app -w 4 -b 134.209.194.133:5000
+gunicorn web_app:app -w 4 -b 127.0.0.1:5000
